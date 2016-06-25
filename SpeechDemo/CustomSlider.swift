3bcf16a99bf23f281537ab0f6ad04b21c1350543
Mark Hamilton <TheDarkCode@users.noreply.github.com>
2016-06-25 05:02:26 -0700

    Add Demo

---
+++ SpeechDemo/CustomSlider.swift
@@ -0,0 +1,27 @@
+//
+//  CustomSlider.swift
+//  SpeechDemo
+//
+//  Created by Gabriel Theodoropoulos on 10/2/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class CustomSlider: UISlider {
+
+    var sliderIdentifier: Int!
+    
+//    init() {
+//        
+//        sliderIdentifier = 0
+//    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        sliderIdentifier = 0
+    }
+
+}
